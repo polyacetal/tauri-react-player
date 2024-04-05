fe83d648125b0921ff204e4a1faab6c2274df50d
polyacetal <yotyan988@gmail.com>
2024-04-05 10:37:49 +0900

    read desktop videos

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,37 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ReactPlayer from 'react-player';
+import {desktopDir, join } from '@tauri-apps/api/path';
+import { convertFileSrc } from '@tauri-apps/api/tauri';
+import "./App.css";
 
-function App() {
-  const [url, setUrl] = useState("https://www.youtube.com/watch?v=RS6x4GBQu-4");
+const App = () => {
+  const [url, setUrl] = useState('');
+  const [src, setSrc] = useState('');
+  const [player, setPlayer] = useState('');
+
+  useEffect(() => {
+	const fn = async () => {
+		const desktopDirPath = await desktopDir();
+		const new_url = convertFileSrc(await join(desktopDirPath, src));
+		setUrl(new_url);
+		const player = <ReactPlayer url={new_url} controls={true} width="100%" height="100%"/>;
+		setPlayer(player);
+	};
+	fn();
+  }, [src])
 
   return (
 		<>
-		<ReactPlayer url={url} controls={true} />
-		<input type="text" value={url} onChange={e => setUrl(e.target.value)} />
+		<h1>React Player</h1>
+		<div class="react-player">
+			{player}
+			<br />
+			{src}
+			<br/>
+			{url}
+			<br />
+			<input type="text" value={src} onChange={e => setSrc(e.target.value)} />
+		</div>
 		</>
   );
 }
